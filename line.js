b8d7a12353679c66d15602e998517f71864797e1
sKawashima <bysk.333@gmail.com>
2019-07-31 11:51:58 +0900

    Fix link url

---
+++ line.js
@@ -0,0 +1,9 @@
+const str = `式場を決める
+https://futalist.mwed.jp/task/1
+
+☑式場決定の条件リストを作る
+☑気になる結婚式場のリストを作る
+☑気になる式場のブライダルフェアに参加する
+☑式場と契約する`
+
+console.log(`https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(str)}`)
